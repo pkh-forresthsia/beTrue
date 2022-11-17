6db67168b81ba967fcd3103f2927fb65b0ae053d
Jesse Tseng <pkh_uuuu1112@photons.com.tw>
2022-11-17 17:59:10 +0800

    local value

---
+++ price.py
@@ -0,0 +1,24 @@
+from basic import *
+import sqlite3
+import sys
+
+sys.path.insert(0,'../data')
+
+class DayInfo(Param):
+    def __init__(self):
+        super().__init__()
+        self.parameter['dataset']="TaiwanStockPrice"
+        self.priceData=sqlite3.connect('priceData')
+    def getPriceData(self,start_date,end_date):
+        self.parameter['start_date']=start_date
+        self.parameter['end_date']=end_date
+        return
+class DayInfoAll(DayInfo):
+    def __init__(self):
+        super().__init__()
+        self.allPriceData=sqlite3.connect('allPriceData')
+    def getAllPriceData(self,start_date):
+        self.parameter['start_date']=start_date
+        return
+
+        
