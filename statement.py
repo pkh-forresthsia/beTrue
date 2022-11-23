5feecfbe7c02bd1296a7768b7b5db015ac78e2a9
Jesse Tseng <pkh_uuuu1112@photons.com.tw>
2022-11-23 18:31:58 +0800

    price Model

--- statement.py
+++ statement.py
@@ -1,6 +1,7 @@
 from basic import *
 import sqlite3
 import sys
+from sympy import *
 
 sys.path.insert(0,'../data')
 
@@ -49,6 +50,30 @@ class StatementManage(Info):
         for i in range(len(summaryData)):
             summaryData['index'].iloc[i]=summaryData['increase'].iloc[i]>0
         return summaryData
+    def discountRate(self,eps,price,g=0.02,e=0.1):
+        tempPrice=0
+        r=(1+g)/(1+e)
+        x=symbols('x')
+        S=solve(eps*x*(1-x**5)+eps*x**5*(1-x)*r/(1-r)-price*(1-x))
+        for i in range(len(S)):
+            if S[i]!=1 and S[i]>0:
+                tempPrice=S[i]*(1+e)-1
+                return tempPrice
+        # return S[0]*(1+e)-1
+        
+    def estimatePrice(self,eps,growth1,growth2=0.02,g=0.02,e=0.1):
+        r=(1+g)/(1+e)
+        tempSum=0
+        for i in range(10):
+            if i<5:
+                eps=eps*((1+growth1)/(1+e))
+                tempSum=tempSum+eps
+            else:
+                eps=eps*((1+growth2)/(1+e))
+                tempSum=tempSum+eps
+        eps=eps*r/(1-r)
+        tempSum=tempSum+eps
+        return tempSum
     def priceModel(self,type):
         typeData=self.getType(type)
         ids=typeData.columns
@@ -59,10 +84,10 @@ class StatementManage(Info):
                 rollingData=self.stockRolling(typeData,stockId)
                 rollingIncreaseData=self.rollingIncrease(rollingData,4)
                 if(len(rollingIncreaseData)>0):
-                    increase=rollingIncreaseData.iloc[0].increase
+                    increase=rollingIncreaseData.iloc[-1].increase
                     tempData.append({
                         'stock_id':stockId,
-                        'increase':round(increase)})
+                        'increaseEPS':round(increase)})
 
         return pd.DataFrame(tempData)
     def increasePeriodData(self,increaseData):
