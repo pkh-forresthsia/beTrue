767c3d84bcdb4be33efb017f31135ddb3c9c3f6d
Jesse Tseng <pkh_uuuu1112@photons.com.tw>
2022-12-07 18:35:50 +0800

    local max

--- dayData.py
+++ dayData.py
@@ -7,4 +7,9 @@ class DayData(Combine):
     def price(self,value):
         data=self.stockPrice.pivot_table(values=value,index='date',columns='stock_id')
         return data
-        
+class LocalValue(DayData):
+    def __init__(self,n):
+        super().__init__()
+        self.n=n
+    def localMax(self,stock_id='TAIEX'):
+        priceMax=self.price('max')['stock_id']            
