f76e7d7946872a4e5a84ca1bbc207d09f8c87bdc
Jesse Tseng <pkh_uuuu1112@photons.com.tw>
2022-12-08 18:32:36 +0800

    local value detail

--- dayData.py
+++ dayData.py
@@ -8,10 +8,9 @@ class DayData(Combine):
         data=self.stockPrice.pivot_table(values=value,index='date',columns='stock_id')
         return data
 class LocalValue(DayData):
-    def __init__(self,n):
+    def __init__(self):
         super().__init__()
-        self.n=n
-    def localValue(self,n,type='max',stock_id="TAIEX"):
+    def getLocal(self,n,type='max',stock_id="TAIEX"):
         priceData=self.price(type)[stock_id].dropna()
         if type=='max':
             priceDataRb=priceData.rolling(n).max()
@@ -19,10 +18,51 @@ class LocalValue(DayData):
             priceDataRb=priceData.rolling(n).min()
         df=pd.concat({'stockPrice':priceData,'rollingBefore':priceDataRb},axis=1)
         df['rollingAfter']=np.NaN
-        df['localValue']='local'+type
         df['rollingAfter']=df['rollingBefore'].shift(-n+1,axis=0)
-        df['localValue']=df['rollingBefore']==df['rollingAfter']
-        return df[df['localValue']==True] 
+        df['localcheck']=df['rollingBefore']==df['rollingAfter']
+        localValue=df[df['localcheck']==True]
+        return  localValue[['stockPrice']]
+    def localConcat(self,n,stock_id="TAIEX"):
+        localMax=self.getLocal(n,'max',stock_id)
+        localMax['localValue']='localMax'
+        localMin=self.getLocal(n,'min',stock_id)
+        localMin['localValue']='localMin'
+        loclaPeriod=pd.concat([localMax,localMin],axis=0).sort_index()
+        return loclaPeriod
+    def localPeriod(self,n,stock_id="TAIEX"):
+        localPeriod=self.localConcat(n,stock_id)
+        localPeriod2=pd.DataFrame([])
+        tempPeriod=localPeriod.iloc[0]
+        tempPrice=localPeriod['stockPrice'][0]
+        for i in range(len(localPeriod)-1):
+            localValue=localPeriod['localValue']
+            if localValue[i]!=localValue[i+1]:
+                localPeriod2=pd.concat([localPeriod2,tempPeriod],axis=1)
+                tempPeriod=localPeriod.iloc[i+1]
+                tempPrice=localPeriod['stockPrice'][i+1]        
+            else:
+                if localValue[i]=='localMin' and localPeriod['stockPrice'][i+1]<tempPrice:
+                    tempPrice=localPeriod['stockPrice'][i+1]
+                    tempPeriod=localPeriod.iloc[i+1]
+                if localValue[i]=='localMax' and localPeriod['stockPrice'][i+1]>tempPrice:
+                    tempPrice=localPeriod['stockPrice'][i+1]
+                    tempPeriod=localPeriod.iloc[i+1]    
+        localPeriod2=pd.concat([localPeriod2,localPeriod.iloc[-1]],axis=1)
+        return localPeriod2.transpose()     
+    def localPeriodDetail(self,n,stock_id="TAIEX"):
+        dm=DateManage()
+        localPeriod=self.localPeriod(n,stock_id)
+        localPeriod['day1']=localPeriod.index
+        localPeriod['day2']=localPeriod['day1'].shift(1)
+        localPeriod['dayDiffer']=0
+        localPeriod['increase%']=(localPeriod['stockPrice']/localPeriod['stockPrice'].shift(1)-1)*100
+        for i in range(1,len(localPeriod)):
+            localPeriod['dayDiffer'][i]=dm.daysDiffer(localPeriod['day1'][i],localPeriod['day2'][i])
+            localPeriod['increase%'][i]=round(localPeriod['increase%'][i])
+        return localPeriod[['stockPrice','localValue','dayDiffer','increase%']][1:]    
+
+
+
 
 
         
