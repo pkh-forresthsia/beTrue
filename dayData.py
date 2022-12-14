bedfdfa00af189c99ea933a995b26278170288ee
unknown <uuuu1112@gmail.com>
2022-12-15 01:39:57 +0800

    local value

--- dayData.py
+++ dayData.py
@@ -1,2 +1,20 @@
 from data import *
 
+class DayData(FromSQL):
+    def __init__(self):
+        super().__init__()
+        self.price=self.datasetFromSQL("TaiwanStockPrice")
+        # self.max=self.data('max')
+        # self.min=self.data('min')
+class LocalValue(DayData):
+    def __init__(self):
+        super().__init__()
+    def getLocal(self,n,type='max',stock_id="TAIEX"):
+        if type=='max':
+            priceData=self.max[stock_id].dropna()
+            priceDataRb=priceData.rolling(n).max()
+        else:
+            priceData=self.min[stock_id].dropna()
+            priceDataRb=priceData.rolling(n).min()
+        df=pd.concat({'stockPrice':priceData,'rollingBefore':priceDataRb},axis=1)
+        return df
