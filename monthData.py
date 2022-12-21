f77162e1850a74c482fb438865f751b68ef48852
unknown <uuuu1112@gmail.com>
2022-12-22 01:13:21 +0800

    revenue data

--- monthData.py
+++ monthData.py
@@ -3,4 +3,8 @@ from data import *
 class MonthData(FromSQL):
     def __init__(self):
         super().__init__()
-        self.revenue=self.datasetFromSQL("TaiwanStockMonthRevenue").pivot_table(index='date',columns='stock_id',values='revenue')    
+        self.revenue=self.datasetFromSQL("TaiwanStockMonthRevenue")  
+    def revenueData(self):
+        revenueData=self.revenue
+        revenueData['date']=revenueData['revenue_year']+"-"+revenueData['revenue_month']+"-10"
+        return revenueData
